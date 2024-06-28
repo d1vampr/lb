c3ac0a765a9945e25aa2f087bac495acee4b13ee
alice(ali) <ali@mail.com>
2024-06-28 16:00:46 +0300

    дабавила твои изменения

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "A + B = " << a + b << '\n'
-        << "A - B = " << a - b << '\n';
-        << "A * B = " << a * b << '\n';
-        << "A / B = " << a / b << '\n';
+    cout  "A + B = "  a + b  '\n'
+         "A - B = "  a - b  '\n'
+         "A * B = "  a * b  '\n'
+         "A / B = "  a / b  '\n'
         max_number = max(numbers)
-            print("Максимум:", max_number)
+        print("Максимум:", max_number)
+        min_number  min(numbers)
+        print("Минимум:", min_number)
 }
